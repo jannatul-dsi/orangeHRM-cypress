b100d30fb019bca74e22cc367d048aeb70574410
jannat-349 <jucse28.349@gmail.com>
2024-09-02 00:18:27 +0600

    handled the unique employee id issue

--- cypress/e2e/orangeHRM.cy.js
+++ cypress/e2e/orangeHRM.cy.js
@@ -1,47 +1,60 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
+const { random } = require("lodash");
 
-describe('template spec', () => {
-  const adminUserName = "Admin"
-  const adminPassword = "admin123"
+describe("template spec", () => {
+  const adminUserName = "Admin";
+  const adminPassword = "admin123";
 
   function login(userName, password) {
-    cy.get("input[name='username'").type(userName)
-    cy.get("input[name='password'").type(password)
-    cy.get("[type='submit']").click()
+    cy.waitTillVisible("input")
+    cy.get("input[name='username'").type(userName);
+    cy.get("input[name='password'").type(password);
+    cy.get("[type='submit']").click();
   }
 
   function getUserName() {
     return {
       firstName: faker.name.firstName(),
-      lastName: faker.name.lastName()
+      lastName: faker.name.lastName(),
     };
   }
-  
 
   function createEmployee() {
-    cy.get("span").contains("PIM").click()
-    cy.waitTillVisible("h6")
-    cy.get("h6").should("have.text","PIM")
-    cy.get("button").contains("Add").click()
-    cy.waitTillVisible("h6")
+    cy.get("span").contains("PIM").click();
+    cy.waitTillVisible("h6");
+    cy.get("h6").should("have.text", "PIM");
+    cy.get("button").contains("Add").click();
+    cy.waitTillVisible("h6");
 
-    const {firstName, lastName} = getUserName()
+    const { firstName, lastName } = getUserName();
 
-    cy.get("input[name='firstName']").type(firstName)
-    cy.get("input[name='lastName']").type(lastName)
+    cy.get("input[name='firstName']").type(firstName);
+    cy.get("input[name='lastName']").type(lastName);
 
-    
-
+    cy.get('label').contains("Employee Id").parent().siblings('div').find('input').clear().type("0303")
+    cy.get("span").contains("Employee Id already exists").should('be.visible').then(() => {
+      const generateUniqueId = () => Math.floor(1000 + Math.random() * 9000).toString();
+  
+      const checkUniqueId = () => {
+          const newId = generateUniqueId();
+          cy.get('label').contains("Employee Id").parent().siblings('div').find('input').clear().type(newId);
+          cy.get("span").contains("Employee Id already exists").should('not.exist')
+              .then(() => {
+                cy.get("input[type='checkbox']").click({ force: true });
+              });
+      };
+  
+      checkUniqueId();
+    })
   }
   before(() => {
-    cy.visit("/")
-    cy.title().should("eq", "OrangeHRM")
-    login(adminUserName, adminPassword)
-  })
-  it('passes', () => {
-    cy.waitTillVisible("h6")
-    cy.get("h6").should("have.text","Dashboard")
-    createEmployee()
-
-  })
-})
+    cy.visit("/");
+    cy.title().should("eq", "OrangeHRM");
+    login(adminUserName, adminPassword);
+  });
+  it("passes", () => {
+    cy.waitTillVisible("h6");
+    cy.get("h6").should("have.text", "Dashboard");
+    createEmployee();
+  });
+});
