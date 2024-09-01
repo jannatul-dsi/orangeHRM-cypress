5c71fcb51a8f1f4de3a83e4ca88c0c52b4a00cc8
jannat-349 <jucse28.349@gmail.com>
2024-09-02 00:28:16 +0600

    employee created successfully

--- cypress/e2e/orangeHRM.cy.js
+++ cypress/e2e/orangeHRM.cy.js
@@ -6,7 +6,7 @@ describe("template spec", () => {
   const adminPassword = "admin123";
 
   function login(userName, password) {
-    cy.waitTillVisible("input")
+    cy.waitTillVisible("input");
     cy.get("input[name='username'").type(userName);
     cy.get("input[name='password'").type(password);
     cy.get("[type='submit']").click();
@@ -16,6 +16,13 @@ describe("template spec", () => {
     return {
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
+    };
+  }
+
+  function generateUsernameAndPassword(firstName, lastName) {
+    return {
+      username: firstName + "123" + lastName,
+      password: firstName + "123" + lastName,
     };
   }
 
@@ -31,21 +38,68 @@ describe("template spec", () => {
     cy.get("input[name='firstName']").type(firstName);
     cy.get("input[name='lastName']").type(lastName);
 
-    cy.get('label').contains("Employee Id").parent().siblings('div').find('input').clear().type("0303")
-    cy.get("span").contains("Employee Id already exists").should('be.visible').then(() => {
-      const generateUniqueId = () => Math.floor(1000 + Math.random() * 9000).toString();
-  
-      const checkUniqueId = () => {
+    const fullName = firstName + " " + lastName;
+
+    cy.get("label")
+      .contains("Employee Id")
+      .parent()
+      .siblings("div")
+      .find("input")
+      .clear()
+      .type("0303");
+    cy.get("span")
+      .contains("Employee Id already exists")
+      .should("be.visible")
+      .then(() => {
+        const generateUniqueId = () =>
+          Math.floor(1000 + Math.random() * 9000).toString();
+        const checkUniqueId = () => {
           const newId = generateUniqueId();
-          cy.get('label').contains("Employee Id").parent().siblings('div').find('input').clear().type(newId);
-          cy.get("span").contains("Employee Id already exists").should('not.exist')
-              .then(() => {
-                cy.get("input[type='checkbox']").click({ force: true });
-              });
-      };
-  
-      checkUniqueId();
-    })
+          cy.get("label")
+            .contains("Employee Id")
+            .parent()
+            .siblings("div")
+            .find("input")
+            .clear()
+            .type(newId);
+          cy.get("span")
+            .contains("Employee Id already exists")
+            .should("not.exist")
+            .then(() => {
+              cy.get("input[type='checkbox']").click({ force: true });
+            });
+        };
+        checkUniqueId();
+        const { username, password } = generateUsernameAndPassword(
+          firstName,
+          lastName
+        );
+        cy.get("label")
+          .contains("Username")
+          .parent()
+          .siblings("div")
+          .find("input")
+          .type(username);
+        cy.get("label")
+          .contains("Password")
+          .parent()
+          .siblings("div")
+          .find("input")
+          .type(password);
+        cy.get("label")
+          .contains("Confirm Password")
+          .parent()
+          .siblings("div")
+          .find("input")
+          .type(password);
+        cy.get("button[type='submit']").click();
+      });
+    cy.get(".oxd-text--toast-message").should(
+      "have.text",
+      "Successfully Saved"
+    );
+    cy.waitTillVisible("h6");
+    cy.get("h6").should("contain.text", fullName);
   }
   before(() => {
     cy.visit("/");
